cc7dc4068c79be5b814e3c3f9189d4bcde12e0a2
DonetsYuliya <donecjuli@gmail.com>
2022-11-14 01:36:22 +0200

    creat lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryCard = creatGalleryItemCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryCard);
+
+function creatGalleryItemCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join(" ");
+}
+
+const gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  sourceAttr: "href";
+  captionType: "attr";
+  captionsData: "alt";
+  captionPosition: "bottom";
+  captionDelay: 250;
+});
+
+// console.log(galleryItems);
